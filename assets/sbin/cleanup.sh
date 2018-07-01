b66ae3cf3b214a5f674f45d7517443327807e3ba
Andrey Usov <andrey.usov@mobalyticshq.com>
2018-07-01 08:29:36 +0300

    [+] removing __pycache__ dirs

--- assets/sbin/cleanup.sh
+++ assets/sbin/cleanup.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
+cleanup_pycache() {
+
+    _PATH=${1:-}
+
+    echo "[INFO] Cleaning __pycache__ dirs for the path: ${_PATH}" && \
+        find ${_PATH} -path '*/__pycache__/*' -delete && \
+        find ${_PATH} -type d -name '__pycache__' -delete
+}
+
 echo '[INFO] Remove temporary files' && \
     rm -rf \
         /tmp/* \
         /root/.cache/* \
         /var/lib/apt/lists/*
 
+echo '[INFO] Remove __pycache__ dirs' && \
+    cleanup_pycache "/usr/lib/python3.6/"
+    # for path in "/usr/lib/python3.6/" \
+    #             "/usr/local/lib/python3.6/dist-packages"
+    # do
+    #     cleanup_pycache ${path}
+    # done 

d6343b6e7f908d4b6eba34c527a533c7cfb70638
Andrey Usov <andrey.usov@mobalyticshq.com>
2018-07-01 10:16:33 +0300

    [*] fixed cleanup

--- assets/sbin/cleanup.sh
+++ assets/sbin/cleanup.sh
@@ -4,8 +4,8 @@ cleanup_pycache() {
 
     _PATH=${1:-}
 
-    echo "[INFO] Cleaning __pycache__ dirs for the path: ${_PATH}" && \
-        find ${_PATH} -path '*/__pycache__/*' -delete && \
+    echo "[INFO] Cleaning __pycache__ dirs for the path: ${_PATH}" 
+    find ${_PATH} -type f -path '*/__pycache__/*' -delete && \
         find ${_PATH} -type d -name '__pycache__' -delete
 }
 
